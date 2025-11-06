5fd48da77ca849d330eb8542d759f3f58824c71b
e2izaveta <kozhenkova.yelizaveta@mail.ru>
2025-11-06 15:46:57 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,1 +1,12 @@
+#15
+s = input()  # читаем строку
 
+# Ищем только буквы и проверяем их порядок
+letters = [(i, char) for i, char in enumerate(s) if char.isalpha()]
+
+for idx in range(1, len(letters)):
+    if letters[idx][1] < letters[idx-1][1]:
+        print(letters[idx][0] + 1)  # +1 потому что в задании номер символа (не индекс)
+        break
+else:
+    print(0)
