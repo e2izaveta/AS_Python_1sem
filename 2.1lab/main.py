1af82869d20c8310990558e83f9726423cd7c2e4
e2izaveta <kozhenkova.yelizaveta@mail.ru>
2025-12-17 00:06:39 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    pass
+#15
+# a) Минимальное и максимальное значение
+d = {'a': 1, 'b': 2, 'c': 3}
+print(f"max: {max(d.values())}, min: {min(d.values())}")
+
+# b) Ключи и значения построчно
+for k, v in d.items():
+    print(f"key: {k}, value: {v}")
+
+# c) Словарь со списками
+d2 = {'x': list(range(11, 21)), 
+      'y': list(range(21, 31)), 
+      'z': list(range(31, 41))}
+for k in d2:
+    print(f"пятый элемент из {k}: {d2[k][4]}")
