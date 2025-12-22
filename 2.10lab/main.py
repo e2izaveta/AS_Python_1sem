086976a887ae5b30d4f46839e09a0ce3411203ef
e2izaveta <kozhenkova.yelizaveta@mail.ru>
2025-12-22 19:45:34 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    pass
+# 15
+# а) Проверка имени
+name = "Liza"
+name = name.strip()
+if not name:
+    raise Exception("Некорректное имя")
+print("Имя введено корректно")
+
+# б) Проверка возраста
+age = 18
+if age < 0 or age > 120:
+    raise Exception("Некорректный возраст")
+print("Возраст введен корректно")
+
+# в) Проверка пола
+gender = "ж"
+gender = gender.strip().lower()
+assert gender in ['м', 'ж'], "Некорректный пол"
+print("Пол введен корректно")
